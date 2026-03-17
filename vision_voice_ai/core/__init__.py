52f10ebde256bd003b18586ac0fa7da058ca60ab
VINEET YADAV <vineet9949@gmail.com>
2026-03-17 11:13:06 +0530

    Delete vision_voice_ai directory

--- vision_voice_ai/core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Core package - Central orchestration and control systems
-"""
-from .orchestrator import MasterOrchestrator
-from .response_generator import ResponseGenerator
-
-__all__ = ["MasterOrchestrator", "ResponseGenerator"]
